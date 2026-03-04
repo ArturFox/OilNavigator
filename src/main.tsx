4d43a71f847dacc2f5da1abe0b3e48ff12accacb
Artur Ivanov <arturfox1317@gmail.com>
2026-03-04 23:37:47 +0500

    deploy2

--- src/main.tsx
+++ src/main.tsx
@@ -12,7 +12,7 @@ import { BrowserRouter } from 'react-router-dom';
 createRoot(document.getElementById('root')!).render(
   
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/studentStartup">
       <Provider store={store}>
         <App />
       </Provider>
